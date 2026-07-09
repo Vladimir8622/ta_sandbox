1e3dca2da71e023efb74a8d2ead9130c42a580f5
Rostislav <rostislavsergeevich27@gmail.com>
2026-07-09 16:36:47 +0300

    sf

--- Engine.py
+++ Engine.py
@@ -1,20 +1,33 @@
-import Data_manager as dm
+import data_management.Data_manager as dm
+import Strategies.MA_cross
+import Broker
+import pandas as pd
 
-# Пока хардкодим. должно сообщаться оптимизатором при запуске скрипта
-param = 0
-
+#Загрузка даты
+Market = "MOEX"
+Active = "adjusted_stock"
+Timeframe = "1d"
+Name = "ABIO.MOEX"
+Start = 2023-08-18
+End = 2026-07-08
 manager = dm.Data_manager()
-data = manager.get_data()
+data = manager.get_data(Market, Active, Timeframe, Name, Start, End)
 
-strategy = Strategy
+#Определяем стратегию
+ma_slow = 200
+ma_fast = 50
+strategy = Strategies.MA_cross(ma_slow, ma_fast)
 
-broker = frbwrybwt
+#Определяем брокера
+broker = Broker(3*10**(-4), 10**(-5))
 
-states = []
+States = []
 
-for i in range(begin, end):
+for i in range(Start, End):
     response = strategy.make(data[:i])
-    new_state = broker.check_response
+    current_state = data['current_state']
+    new_state = broker.check_response()
+    broker.check_position
     states.append(new_state)
 
 # analysis
